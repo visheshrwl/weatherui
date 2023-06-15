e5168790b3502c95d24b16c9bcaa8721d0059f51
VisheshRawal <vp2005rawal@gmail.com>
2023-06-15 18:42:16 +0530

    Changed HTML/CSS code to ReactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import './Assets/background.png';
+import logo from './Assets/icon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.card}>
+        <h4>NEW DELHI</h4>
+        <h6>PARTLY CLOUDY</h6>
+        <img src={logo} alt="icon" />
+        <h1>32<sup>&deg;</sup></h1>
+        <div className={styles.container}>
+            <div className="details">
+                <p>max</p>
+                <span>32<sup>&deg;</sup></span>
+            </div>
+            <div className={styles.details}>
+                <p>min</p>
+                <span>30<sup>&deg;</sup></span>
+            </div>
+        </div>
     </div>
   );
 }
